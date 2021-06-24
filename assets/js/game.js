215628ea7ec43249831219cc8e4947c3363e9435
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-06-24 14:04:31 -0600

    name info

---
+++ assets/js/game.js
@@ -0,0 +1,21 @@
+// this creates a function names "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10+10);
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// fight();
